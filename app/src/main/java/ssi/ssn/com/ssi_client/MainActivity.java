547b2e8cb152a690b5fa8d7b5908aae7a0375e4b
Simon Wünsch <simon-wuensch@gmx.de>
2016-09-14 14:57:07 +0200

    Initial commit

---
+++ app/src/main/java/ssi/ssn/com/ssi_client/MainActivity.java
@@ -0,0 +1,13 @@
+package ssi.ssn.com.ssi_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
